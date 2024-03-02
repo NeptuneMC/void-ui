3006bb48efcbd9a9971f88e4b9174f16c8b1b981
marc <marc.deike@outlook.de>
2024-03-02 23:06:54 +0100

    some dsl features

--- src/main/kotlin/com/neptuneclient/voidui/ui/Component.kt
+++ src/main/kotlin/com/neptuneclient/voidui/ui/Component.kt
@@ -1,7 +1,21 @@
 package com.neptuneclient.voidui.ui
 
-sealed class Component {
+sealed class Component(
+    var x: Int? = null,
+    var y: Int? = null,
+    width: Int? = null,
+    height: Int? = null
+) {
+
+    var width = width ?: 0
+    var height = height ?: 0
 
     abstract fun build(): Component
+
+    val Number.cw
+        get() = (this.toDouble() / 100) * width
+
+    val Number.ch
+        get() = (this.toDouble() / 100) * height
 
 }
--- src/main/kotlin/com/neptuneclient/voidui/ui/Screen.kt
+++ src/main/kotlin/com/neptuneclient/voidui/ui/Screen.kt
@@ -2,6 +2,15 @@ package com.neptuneclient.voidui.ui
 
 abstract class Screen {
 
+    var width = 0
+    var height = 0
+
     abstract fun build(): Component
+
+    val Number.vw
+        get() = (this.toDouble() / 100) * width
+
+    val Number.vh
+        get() = (this.toDouble() / 100) * height
 
 }
