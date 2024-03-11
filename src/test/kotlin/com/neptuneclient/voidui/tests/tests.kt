65f666de43fd4ec5e42cbbe39f09394b83bd99dd
Simon E.W <simon.ew.ew@gmail.com>
2024-03-11 19:30:48 +0100

    Implement MockRenderer

--- src/test/kotlin/com/neptuneclient/voidui/tests/tests.kt
+++ src/test/kotlin/com/neptuneclient/voidui/tests/tests.kt
@@ -3,6 +3,8 @@ package com.neptuneclient.voidui.tests
 import com.neptuneclient.voidui.VoidUI
 import com.neptuneclient.voidui.utils.Font
 import org.junit.jupiter.api.Test
+import org.lwjgl.glfw.GLFW
+import java.awt.Color
 import java.nio.file.Path
 
 class FontTest() {
@@ -11,8 +13,18 @@ class FontTest() {
 
     @Test
     fun byteBufferTest() {
-        val font = Font(void, "test", Path.of("fonts/WorkSans-Regular.ttf"), 10)
+        val font = Font(void, "test", Path.of("fonts/WorkSans-Regular.ttf"), 30)
         println(font.data.toString())
+
+        val renderer = void.renderer as MockRenderer
+        while (!GLFW.glfwWindowShouldClose(renderer.window)) {
+            renderer.beginFrame()
+            renderer.roundedRectangle(0f, 0f, 100f, 100f, 20f, Color.RED)
+            renderer.text(100f, 100f, "Hello, World!", font, Color.WHITE)
+            renderer.endFrame()
+        }
+
+        renderer.destroy()
     }
 
 }
