9ee25dc7473ee871645f5027e122a77ea02455fd
marc <marc.deike@outlook.de>
2024-03-20 15:47:26 +0100

    Group is a widget now

--- src/main/kotlin/com/neptuneclient/voidui/widgets/elements.kt
+++ src/main/kotlin/com/neptuneclient/voidui/widgets/elements.kt
@@ -5,23 +5,6 @@ import java.awt.Color
 
 // TODO add docs to this file
 
-class Group(private val children: Array<Widget>) : Element() {
-    
-    override fun init(screen: Screen, parent: Widget?) {
-        val size = sizeSelf(screen, parent)
-        x = size.x
-        y = size.y
-        width = size.width
-        height = size.height
-        
-        children.forEach { it.init(screen, this) }
-    }
-    
-    override fun render(renderer: Renderer) {
-    }
-
-}
-
 class Panel : Element() {
     
     override fun render(renderer: Renderer) {
--- src/main/kotlin/com/neptuneclient/voidui/widgets/widgets.kt
+++ src/main/kotlin/com/neptuneclient/voidui/widgets/widgets.kt
@@ -1,2 +1,27 @@
 package com.neptuneclient.voidui.widgets
 
+/**
+ * An internal widget which is used to bundle other widgets.
+ *
+ * @param children An array with all chidren widgets.
+ */
+internal class Group(private val children: Array<Widget>) : Widget() {
+
+    override fun init(screen: Screen, parent: Widget?) {
+        val size = sizeSelf(screen, parent)
+        x = size.x
+        y = size.y
+        width = size.width
+        height = size.height
+
+        children.forEach { it.init(screen, this) }
+    }
+
+    /**
+     * A group doesn't build it's own widget, so this return type can be ignored.
+     */
+    override fun build(): Widget {
+        return this
+    }
+
+}
