49e9f8ee75ad335ef15317f8c9feb07da328d0e7
marc. <marc.deike@outlook.de>
2024-05-03 16:30:36 +0200

    oopsies

--- test-mod/src/main/java/com/neptuneclient/voidui/testmod/ModEntry.java
+++ test-mod/src/main/java/com/neptuneclient/voidui/testmod/ModEntry.java
@@ -1,18 +1,8 @@
 package com.neptuneclient.voidui.testmod;
 
-import com.neptuneclient.voidui.VoidUI;
-import com.neptuneclient.voidui.testmod.example.TestScreen;
-import com.neptuneclient.voidui.testmod.example.TestTheme;
-import com.neptuneclient.voidui.testmod.impl.RendererImpl;
-import com.neptuneclient.voidui.testmod.impl.ScreenBridge;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static net.minecraft.server.command.CommandManager.literal;
 
 public class ModEntry implements ClientModInitializer {
 
